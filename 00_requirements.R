12fa2b7897ffd5ac9e2a0f9b15ec68ad23aa3f05
puppy7777 <whe12345678910@gmail.com>
2025-11-06 22:35:16 -0800

    finished data cleaning
    i hate working with dates

--- 00_requirements.R
+++ 00_requirements.R
@@ -2,3 +2,7 @@ if (require(tidyverse)) {
   install.packages("tidyverse") #for lubridate for dealing with dates
   library(tidyverse)            
 }
+if (require(data.table)) {
+  install.packages("data.table")
+  library(data.table)
+}
